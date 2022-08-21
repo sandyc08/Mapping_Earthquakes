02e86bed86199ca56cad726f42d8c48c1bc55a94
Sandy Cisneros <sandyc08@yahoo.com>
2022-08-20 18:48:23 -0700

    adding multiple markers

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -5,12 +5,20 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
-// Add a marker to the map for Los Angeles, CA 
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: "#ffffa1"
-}).addTo(map);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: "orange"
+    })
+    .bindPopup("<h2>" + city.city + " ," + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
